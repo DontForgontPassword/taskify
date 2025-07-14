478d8cf74821f2a8c12b576eb1ef1116878589ec
DontForgontPassword <tendoffs@gmail.com>
2025-07-14 13:06:29 +0200

    added remove\edit button for task

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,5 +1,8 @@
 import styles from "./TodoItem.module.scss"
 
+import { FiTrash } from "react-icons/fi";
+import { MdEdit } from "react-icons/md";
+
 type Props = {
     task: string,
 }
@@ -7,8 +10,12 @@ type Props = {
 const TodoItem = (Props: Props) => {
     return (
         <li className={styles.todoItem}>
-            <p className={styles.todoTask}>{Props.task}</p>
-        </li>
+            <p className={styles.taskText}>{Props.task}</p>
+            <div className={styles.action}>
+                <button className={styles.editButton}><MdEdit color="white" /></button>
+                <button className={styles.removeButton}><FiTrash color="white" /></button>
+            </div>
+        </li >
     );
 }
 
