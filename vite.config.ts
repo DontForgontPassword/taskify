7ee83458e953fa67b6d13b6985beeda8bf0e3901
DontForgontPassword <tendoffs@gmail.com>
2025-07-20 14:00:23 +0200

    responsive desing

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(),
   ],
-  root: "",
-  base: "/",
+  base: "/taskify/",
   resolve: {
     alias: {
       "@": "/src/",
